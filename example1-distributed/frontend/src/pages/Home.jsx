e5ecd639c4d507a06cafb98ab0ea1727ee45de2e
LizPillajo <ldpillajo@uce.edu.ec>
2026-04-15 18:56:41 -0500

    change: the alert is running in docker

--- example1-distributed/frontend/src/pages/Home.jsx
+++ example1-distributed/frontend/src/pages/Home.jsx
@@ -31,7 +31,7 @@ export const Home = () => {
     return (
       <Box sx={{ padding: 4 }}>
         <Alert severity="error" variant="filled" sx={{ backgroundColor: '#0d2149', color: 'white' }}>
-          Error de conexión: {error.message}. ¡Asegúrate de que tu backend esté encendido en Docker!
+          Error de conexión: {error.message}. ¡Are you sure that your backend is running in Docker!
         </Alert>
       </Box>
     );
