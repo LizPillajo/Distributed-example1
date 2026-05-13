c1018645b0de7a9bef593d493b6d4979b890e501
LizPillajo <ldpillajo@uce.edu.ec>
2026-05-13 14:35:48 -0500

    style: The word “PostgreSQL” was removed from the bookmarks page. Test 1.

--- example1-distributed/frontend/src/pages/Favorites.jsx
+++ example1-distributed/frontend/src/pages/Favorites.jsx
@@ -13,7 +13,7 @@ export const Favorites = () => {
   return (
     <Box sx={{ padding: 4 }}>
       <Typography variant="h3" gutterBottom fontWeight="bold" color="#efb034">
-        My Favorites (PostgreSQL)
+        My Favorites
       </Typography>
       
       <Grid container spacing={3}>
